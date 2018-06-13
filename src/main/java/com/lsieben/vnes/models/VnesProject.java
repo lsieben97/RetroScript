970a811b589ab2b8de2f87c0b7e1644a5e53a125
Luc Sieben <lucsieben97@hotmail.com>
2018-06-13 20:40:44 +0200

    Add description, author, version and license fields to project definition.

--- src/main/java/com/lsieben/vnes/models/VnesProject.java
+++ src/main/java/com/lsieben/vnes/models/VnesProject.java
@@ -3,6 +3,10 @@ package com.lsieben.vnes.models;
 public class VnesProject {
 
     private String name;
+    private String description;
+    private String author;
+    private String license;
+    private String version;
     private String projectFolder;
     private String sourceDir;
     private String[] libraries;
@@ -46,5 +50,21 @@ public class VnesProject {
         return "Name:\t\t\t" + this.name + "\n" +
                 "Project folder:\t" + this.projectFolder + "\n" +
                 "Source folder:\t" + this.sourceDir;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public String getVersion() {
+        return version;
     }
 }
